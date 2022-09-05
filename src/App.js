2004c8cf05cdbe2156735adae2be7c2b48e2715a
Ramazan USLU <ruslu1720@gmail.com>
2022-09-05 11:53:05 +0300

    React Router Dom eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React</h1>
+    <div>
+       <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
     </div>
   );
 }
