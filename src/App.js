48a395230cd0176e420b0bb18aca35f2f07d9a29
Ramazan USLU <ruslu1720@gmail.com>
2022-09-05 12:15:19 +0300

    Navbar componenti olusturuldu & Fontawesome eklendi

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Header />
+      <Navbar />
       <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
     </>
   );
