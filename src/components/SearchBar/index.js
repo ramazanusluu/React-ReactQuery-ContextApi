971664200eca69795b4163196c5904e02862c4ca
Ramazan USLU <ruslu1720@gmail.com>
2022-09-06 15:50:53 +0300

    Searcbar 1. yol

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
 
-function SearchBar() {
+function SearchBar({ data }) {
+  const [filteredData, setFilteredData] = useState([]);
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    const newFilter = data.Result.TreeList.filter((value) => {
+      return value.DisplayName.toLowerCase().includes(searchWord.toLowerCase());
+    });
+   
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
   return (
-    <div>
-      <h1>SearchBar</h1>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="input-group my-5">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Ürün, kategori, servis, mağaza ara"
+                onChange={handleFilter}
+              />
+              <button className="btn btn-danger" type="button">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </button>
+            </div>
+            {filteredData.length !== 0 && (
+              <div>
+                {filteredData.map((item, key) => (
+                  <div key={key}>
+                    <Link to={`/category/${item.ID}`}>
+                      <h4> {item.DisplayName}</h4>
+                    </Link>
+                    {item.SubCategoryList.map((item2, key2) => (
+                      <div key={key2}>
+                        <Link to={`/products/${item2.ID}`}>
+                          <h6>{item2.DisplayName}</h6>
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import SearchBar from "../../components/SearchBar";
+import Loading from "../../components/Loading";
+import { useQuery } from "react-query";
+import { fetchCategoryList } from "../../api";
 
 function Search() {
+  const { isLoading, error, data } = useQuery("category", fetchCategoryList);
+
+  if (isLoading) return <Loading />;
+
+  if (error) return "An error has occurred: " + error.message;
+
+  console.log(data);
   return (
     <div>
-      <SearchBar />
+      <SearchBar data={data} />
     </div>
   );
 }
