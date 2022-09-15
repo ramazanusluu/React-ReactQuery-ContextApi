0269dd3b1ee3a6dad079a8bfce1b5a7957cdb8bd
Ramazan USLU <ruslu1720@gmail.com>
2022-09-15 09:08:17 +0300

    Hata duzeltildi

--- src/contexts/CompareContext.js
+++ src/contexts/CompareContext.js
@@ -6,24 +6,44 @@ const CompareProvider = ({ children }) => {
   // items => karşılaştırma kısmına atılmış ürün olarak düşünülebilir, varsayılan olarak eleman yok
   const [compareItems, setCompareItems] = useState([]);
 
-  const addToCompare = (item) => {
+  const addToCompare = (data) => {
     // if (!findCompareItem) {
     //   return setCompareItems((compareItems) => [item, ...compareItems]);
     // }
 
-    const itemIndex = compareItems.findIndex(
-      (items) => items.ProductType !== item.ProductType
-    );
-    if (itemIndex === -1) {
-      setCompareItems((compareItems) => [item, ...compareItems]);
-    } else {
-      setCompareItems([]);
-    }
+    // const itemIndex = compareItems.findIndex(
+    //   (items) => items.ProductType !== data.ProductType
+    // );
+    // if (itemIndex === -1) {
+    //   setCompareItems((compareItems) => [data, ...compareItems]);
+    // } else {
+    //   setCompareItems([]);
+    // }
 
     // const filtered = compareItems.filter(
     //   (item) => item.ID !== findCompareItem.ID
     // );
     // setCompareItems(filtered);
+
+    const itemIndex = compareItems.findIndex(
+      (items) => items.ProductType !== data.ProductType
+    );
+
+    if (itemIndex === -1) {
+      const findCompare = compareItems.find(
+        (compareItem) => compareItem.ID === data.ID
+      );
+
+      if (!findCompare) {
+        return setCompareItems((compareItems) => [data, ...compareItems]);
+      }
+      const filtered = compareItems.filter(
+        (item) => item.ID !== findCompare.ID
+      );
+      setCompareItems(filtered);
+    } else {
+      setCompareItems([]);
+    }
   };
 
   const values = {
